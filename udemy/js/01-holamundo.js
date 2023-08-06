049eae08db4a99ba31a5b769bdd14bd7d9a64f4e
Alex Lirola Fenoy <alirola-@c1r15s4.42malaga.com>
2023-08-06 20:38:24 +0200

    hola

--- udemy/js/01-holamundo.js
+++ udemy/js/01-holamundo.js
@@ -1,4 +1,8 @@
+
 alert('hola mundo con JS');
 alert('Bienvenido al curso');
+/*
 document.write('Hola mundo desde fichero externo');
 console.log('Muestra esto en la consola');
+*/
+
---
+++ udemy/js/02-variables.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_y_continente = pais+' '+continente;
+//alert(pais_y_continente);
+let prueba = "hola";
